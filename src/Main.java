87fe07f9897e054704dfef6efdbdcd8648f5a25e
Gangula Sandaru Dinusantha <gangula.sandaru@gmail.com>
2023-04-10 22:06:49 +0530

    implimented the string special character remover and also number remover

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,17 @@ import java.util.Scanner;
 
 public class Main {
 
+    // special and number remover function.
+    public String stringRemover(String text){
+        String stringRm;
+        stringRm = text.replaceAll("[^a-zA-Z]","");
+        return stringRm;
+    }
+
     public static void main(String[] args){
+        Main obj = new Main();
+        // store file data
+        String file_data = "";
 
         // reading the file
         File reading_file = new File("K:/Computer Science (My life)/My Work Station/DSA-Mini project/src/file7.txt");
@@ -15,11 +25,14 @@ public class Main {
             Scanner scan = new Scanner(reading_file);
             // reading the whole file line by line
             while(scan.hasNextLine()){
-                System.out.println(scan.nextLine());
+                file_data = file_data.concat(scan.nextLine() + "\n");
             }
 
         }catch (IOException e){
             System.out.println("File not fund!");
         }
+
+        // print the file data
+        System.out.println(obj.stringRemover(file_data));
     }
 }
