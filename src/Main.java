3e35d552374825b02bc3edb453a17fb75d8a88e4
Gangula Sandaru Dinusantha <gangula.sandaru@gmail.com>
2023-04-10 21:35:16 +0530

    implimented the file reading function

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,25 @@
+import java.io.File;
+import java.io.IOError;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args){
-        System.out.println("Hi gangula.");
+
+        // reading the file
+        File reading_file = new File("K:/Computer Science (My life)/My Work Station/DSA-Mini project/src/file7.txt");
+
+        // catch if there is no file.
+        try{
+            Scanner scan = new Scanner(reading_file);
+            // reading the whole file line by line
+            while(scan.hasNextLine()){
+                System.out.println(scan.nextLine());
+            }
+
+        }catch (IOException e){
+            System.out.println("File not fund!");
+        }
     }
 }
