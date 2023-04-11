8161f339ad9bd809d2fb0a3375f15bb9201446fa
Gangula Sandaru Dinusantha <gangula.sandaru@gmail.com>
2023-04-11 22:10:07 +0530

    improve the code for the string remover

--- src/DataModifiers.java
+++ src/DataModifiers.java
@@ -1,43 +1,27 @@
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 class DataModifiers {
 
     // special and number remover function.
-    String[] stringRemover(String text) {
-        // todo ignore the case. fix this
-        String stringRm;
-        // store split data in the list form
-        String[] modifiedDataList;
-        // replace with white space , remove special characters , numbers using regular expression
-        stringRm = text.replaceAll("[^a-zA-Z]+", " ");
-
-        // split the words
-        modifiedDataList = stringRm.split(" ");
-
-        String[] uniqueDataList = new String[modifiedDataList.length];
-        int uniqueDataCount = 0;
-        boolean[] seen = new boolean[modifiedDataList.length];
-
-        for (int i = 0; i < modifiedDataList.length; i++) {
-            String currentWord = modifiedDataList[i].replaceAll("[\\s+]", "");
-
-            if (!seen[i]) {
-                uniqueDataList[uniqueDataCount++] = currentWord;
-                seen[i] = true;
-            }
-
-            for (int j = i + 1; j < modifiedDataList.length; j++) {
-                if (!seen[j] && currentWord.equalsIgnoreCase(modifiedDataList[j].replaceAll("[\\s+]", ""))) {
-                    seen[j] = true;
-                }
+    public static String[] stringRemover(String text) {
+        // Replace non-alphabetic characters with whitespace and split into words
+        String[] words = text.replaceAll("[^a-zA-Z]+", " ").split("\\s+");
+        // Create a HashSet to store unique words
+        Set<String> uniqueWords = new HashSet<>();
+        // Loop through words
+        for (String word : words) {
+            // If word is not already in uniqueWords and is not identical to an existing word (ignoring case),
+            // add it to the uniqueWords set
+            if (!uniqueWords.contains(word) && !uniqueWords.contains(word.toLowerCase()) && !uniqueWords.contains(word.toUpperCase())) {
+                uniqueWords.add(word);
             }
         }
-
-        // Trim the uniqueDataList to remove null elements
-        uniqueDataList = Arrays.copyOf(uniqueDataList, uniqueDataCount);
-
-        return uniqueDataList;
+        // Convert uniqueWords set back to an array and return
+        return uniqueWords.toArray(new String[uniqueWords.size()]);
     }
+
 
 //    // similar word catcher
 //    String[] wordCatcher(String[] word_list){
