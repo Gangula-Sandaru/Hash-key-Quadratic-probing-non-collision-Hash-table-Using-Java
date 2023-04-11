ffe2d81d31444ca95354e74920015d06d4cac028
Gangula Sandaru Dinusantha <gangula.sandaru@gmail.com>
2023-04-11 16:47:46 +0530

    implimenting the space remover

--- src/DataModifiers.java
+++ src/DataModifiers.java
@@ -1,10 +1,25 @@
 class DataModifiers {
 
     // special and number remover function.
-    String stringRemover(String text){
+    String[] stringRemover(String text){
         String stringRm;
+        // store split data in the list form
+        String[] modifiedDataList, finalList = new String[];
+        int count = 0;
+
         stringRm = text.replaceAll("[^a-zA-Z]"," ");
-        return stringRm;
+
+        // split the words
+        modifiedDataList = stringRm.split(" ");
+
+        for(int i = 0; i <= (modifiedDataList.length) - 1; i++){
+            if(!modifiedDataList[i].equals(" ")){
+                finalList[count] = modifiedDataList[i];
+                count++;
+            }
+        }
+
+        return finalList;
     }
 
     // similar word catcher
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,9 @@
 
 public class Main {
     public static void main(String[] args){
-        // store modified data
-        String modifiedData;
-        // store split data in the list form
-        String[] modifiedDataList;
+        // store modified data in the list
+        String[] modifiedData;
+
 
         // create file reader obj and pass the path
         FileReader reader = new FileReader("K:/Computer Science (My life)/My Work Station/DSA-Mini project/src/file7.txt");
@@ -14,10 +13,11 @@ public class Main {
         // read the file and remove the special and number characters then store it in the modified data variable
         modifiedData = modifiers.stringRemover(reader.Read_file());
 
-        // split the words
-        modifiedDataList = modifiedData.split(" ");
-        System.out.println(modifiedData);
-        System.out.println(modifiedDataList.toString());
+
+
+        for(int i = 0; i <= (modifiedData.length) -1 ; i++){
+            System.out.println(modifiedData[i]);
+        }
 
 
     }
